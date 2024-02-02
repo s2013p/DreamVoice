6a2d5ebe7cbcab28debb0ba2c1e2673124d9cbe3
Sasza Pohl <sp2013@proton.me>
2024-02-02 15:34:03 +0100

    1.5

--- tts.py
+++ tts.py
@@ -28,22 +28,22 @@ def say(text, lang='pl'):
             
             # Usunięcie tymczasowego pliku
             os.remove(temp_filename)
-        except:
-            import pyttsx3
+    except:
+        import pyttsx3
 
-            def say(text, lang='pl'):
-                engine = pyttsx3.init()
+        def say(text, lang='pl'):
+            engine = pyttsx3.init()
 
-                # Ustaw język, pyttsx3 może wymagać dodatkowej konfiguracji dla różnych języków
-                voices = engine.getProperty('voices')
-                for voice in voices:
-                    # Sprawdź dostępne języki w głosie (uwaga: 'languages' może zwracać listę kodów języków)
-                    if lang in voice.languages:
-                        engine.setProperty('voice', voice.id)
-                        break
+            # Ustaw język, pyttsx3 może wymagać dodatkowej konfiguracji dla różnych języków
+            voices = engine.getProperty('voices')
+            for voice in voices:
+                # Sprawdź dostępne języki w głosie (uwaga: 'languages' może zwracać listę kodów języków)
+                if lang in voice.languages:
+                    engine.setProperty('voice', voice.id)
+                    break
 
 
-                # Odtwarzanie tekstu
-                engine.say(text)
-                engine.runAndWait()
+            # Odtwarzanie tekstu
+            engine.say(text)
+            engine.runAndWait()
 
